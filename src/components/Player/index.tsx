c0921c84da2c7d55462f55ce42d46db27a13e0c7
caduxl007 <eduardo2020dev@gmail.com>
2021-04-20 23:58:33 -0300

    initializing component for player

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.scss';
+
+export function Player() {
+  return (
+    <div className={styles.playerContainer}>
+      <header>
+        <img src="/playing.svg" alt="Tocando agora"/>
+        <strong>Tocando agora</strong>
+      </header>
+    </div>
+  )
+}
